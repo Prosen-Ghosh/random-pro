b2d3fa92efe6e310d6b04cbc8d2521579d0c57ad
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-08-26 15:57:22 +0600

    package version updated

---
+++ src/pro-users.js
@@ -0,0 +1,32 @@
+var names = require('./../data/names');
+
+var randGenerateUserName = (gender) => {
+    gender = gender || "MALE";
+    locNames = gender.toUpperCase() == "MALE" ? names.firstNameMale : names.firstNameFemale;
+    return locNames[Math.floor(Math.random()*locNames.length)];
+}
+
+var randGenerateLastName = () => {
+    return names.lastName[Math.floor(Math.random()*names.lastName.length)];
+}
+
+var randGenerateEmail = function(){
+    return randGenerateUserName().toLowerCase() + 
+           randGenerateLastName().toLowerCase() + 
+           "@" + 
+           names.emailDomain[Math.floor(Math.random() * names.emailDomain.length)].toLowerCase();
+}
+// randGenerateEmail.prototype.with = function(firstName,lastName){
+//     return firstName + lastName + names[Math.floor(Math.random() * names.emailDomain.length)];
+// }
+
+var randGenerateFullName = function(gender){
+    var locNames = gender.toUpperCase() == "MALE" ? names.firstNameMale : names.firstNameFemale;
+    return locNames[Math.floor(Math.random()*locNames.length)] + names.lastName[Math.floor(Math.random()*names.lastName.length)];
+}
+module.exports = {
+    randGenerateUserName : randGenerateUserName,
+    randGenerateLastName : randGenerateLastName,
+    randGenerateEmail : randGenerateEmail,
+    randGenerateFullName : randGenerateFullName
+};
