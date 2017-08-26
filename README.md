2c86a787989deaad6c6cd4433d789dc71001d215
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-08-26 15:55:22 +0600

    added pro-users and data for pro-users

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # random-pro
 A JavaScript package for random things.
+
+#Installation
+```npm install random-pro```
+
+#Number
+```javascript
+var rand = require('random-pro');
+console.log(rand.generateNumber());
+//
+```
+#License
+MIT
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var numberPro = require('./src/pro-number.js');
-
+var usersPro = require('./src/pro-users');
+console.log(usersPro.randGenerateEmail());
 module.exports = {
-    randNumber : numberPro.randNumber
+    generateNumber : numberPro.randNumber,
+    generateName : usersPro.randGenerateUserName,
+    generateLastName : usersPro.randGenerateLastName,
+    generateFullName : usersPro.randGenerateFullName,
+    generateEmail : usersPro.randGenerateEmail
 }
