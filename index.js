950718a36153773f8758ce335745d8214ca508d1
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-08-29 20:57:03 +0600

    browser support

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 var numberPro = require('./src/pro-number.js');
 var usersPro = require('./src/pro-users');
 var stringPro = require('./src/pro-string');
-module.exports = {
-    generateNumber : numberPro.randNumber,
-    generateEvenNumber : numberPro.randEvenNumber,
-    generateOddNumber : numberPro.randOddNumber,
-    generateName : usersPro.randGenerateUserName,
-    generateLastName : usersPro.randGenerateLastName,
-    generateFullName : usersPro.randGenerateFullName,
-    generateEmail : usersPro.randGenerateEmail,
-    generateString : stringPro.generateString,
-    generateToken : stringPro.generateToken,
-    generatePassword : usersPro.generatePassword,
-    generateBirthDate : usersPro.generateBirthDate
-}
+
+(function(){
+    if(typeof module !== 'undefined' && module.exports){
+        module.exports = {
+            generateNumber : numberPro.randNumber,
+            generateEvenNumber : numberPro.randEvenNumber,
+            generateOddNumber : numberPro.randOddNumber,
+            generateName : usersPro.randGenerateUserName,
+            generateLastName : usersPro.randGenerateLastName,
+            generateFullName : usersPro.randGenerateFullName,
+            generateEmail : usersPro.randGenerateEmail,
+            generateString : stringPro.generateString,
+            generateToken : stringPro.generateToken,
+            generatePassword : usersPro.generatePassword,
+            generateBirthDate : usersPro.generateBirthDate
+        }
+    }
+    else {
+        window.rand = {
+            generateNumber : numberPro.randNumber,
+            generateEvenNumber : numberPro.randEvenNumber,
+            generateOddNumber : numberPro.randOddNumber,
+            generateName : usersPro.randGenerateUserName,
+            generateLastName : usersPro.randGenerateLastName,
+            generateFullName : usersPro.randGenerateFullName,
+            generateEmail : usersPro.randGenerateEmail,
+            generateString : stringPro.generateString,
+            generateToken : stringPro.generateToken,
+            generatePassword : usersPro.generatePassword,
+            generateBirthDate : usersPro.generateBirthDate
+        }
+    }
+})();
